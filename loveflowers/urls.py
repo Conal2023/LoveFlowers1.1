93e80e8a4fa04b5064c63cdf266de27bf5695aa1
Conal McCrory <conalmccrory@gmail.com>
2024-07-01 18:57:55 +0000

    Add 404 page

--- loveflowers/urls.py
+++ loveflowers/urls.py
@@ -18,6 +18,7 @@ from contact import views as contact_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
