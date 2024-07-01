5b345529012ae2f01acc3037a2b2a2a759ae1ab7
Conal McCrory <conalmccrory@gmail.com>
2024-07-01 18:45:35 +0000

    Flake8 tidy up part 2

--- contact/forms.py
+++ contact/forms.py
@@ -10,7 +10,13 @@ class ContactForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'placeholder': 'Name'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Email Address'}),
             'phone': forms.TextInput(attrs={'placeholder': 'Phone Number'}),
-            'mode_of_contact': forms.Select(attrs={'placeholder': 'Contact by'}),
-            'question_categories': forms.Select(attrs={'placeholder': 'How can we help you?'}),
-            'message': forms.Textarea(attrs={'placeholder': 'Your message (max 3000 characters)'}),
+            'mode_of_contact': forms.Select(
+                attrs={'placeholder': 'Contact by'}
+                ),
+            'question_categories': forms.Select(
+                attrs={'placeholder': 'How can we help you?'}
+                ),
+            'message': forms.Textarea(
+                attrs={'placeholder': 'Your message (max 3000 characters)'}
+                ),
         }
