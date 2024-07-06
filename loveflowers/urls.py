73ae02a0b54a58e9ed5212d40e040bf9a928bd24
Conal McCrory <conalmccrory@gmail.com>
2024-07-06 18:38:07 +0000

    404 fix

--- loveflowers/urls.py
+++ loveflowers/urls.py
@@ -18,6 +18,7 @@ from contact import views as contact_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
