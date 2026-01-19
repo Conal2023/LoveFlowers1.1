ebd3bbe63b00381b397fd13f7ce759ece018fe31
Conal <conalmccrory@gmail.com>
2026-01-19 20:10:44 +0000

    Remove Cloudinary, use S3 for media

--- loveflowers/settings.py
+++ loveflowers/settings.py
@@ -243,14 +243,6 @@ else:
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 # Keep MEDIA_URL for compatibility
 MEDIA_URL = '/media/'
 
